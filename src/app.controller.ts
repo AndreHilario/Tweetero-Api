1e1f9b1a6ebbd1f535469fa08e06baf2722f5df0
Andre Hilario <andre18hilario@gmail.com>
2023-08-18 13:47:15 -0300

    feat: sing-up with @Post and validation working

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post("sign-up")
+  @HttpCode(200)
+  signUp(@Body() body: CreateUserDto): number {
+    return this.appService.singup(body)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  private users: User[];
+  
+  constructor() {
+    this.users = [];
+  }
+
+  singup(body: CreateUserDto) {
+    const { username, avatar } = body;
+    return this.users.push(new User(username, avatar));
   }
 }
