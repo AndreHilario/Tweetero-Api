692d526a615aec3671443329d67833769b5c67bb
Andre Hilario <andre18hilario@gmail.com>
2023-08-19 12:42:10 -0300

    feat: CORS implemented

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { Tweet } from './entities/tweet.entity';
